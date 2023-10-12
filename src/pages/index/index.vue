38b8b7dac3c400204d8bded892fce43a94ec44ee
ZhangLingKun <zhanglingkun@outlook.com>
2023-10-12 10:31:53 +0800

    add: first edit

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -6,10 +6,18 @@
     <h4>Hello</h4>
     <h5>Hello</h5>
     <br />
-    <u-button type="primary">Hello</u-button>
+    <u-button type="primary" @click="handleClick()">Hello</u-button>
   </view>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// 点击事件
+const handleClick = () => {
+  uni.showToast({
+    title: 'Hello',
+    icon: 'none'
+  })
+}
+</script>
 <style scoped lang="scss">
 .map-warp {
   position: relative;
